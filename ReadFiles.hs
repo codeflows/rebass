9dc6dc97d908139453fced12bf9f340f4c0a153e
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-13 22:15:48 +0200

    refactor

--- ReadFiles.hs
+++ ReadFiles.hs
@@ -7,23 +7,19 @@ import System.Posix.Files(getFileStatus, fileSize)
                 
 ignored = [".", "..", ".git"]
 
-readDirectoryStatus :: Path -> IO File
-readDirectoryStatus path = do      
-    contents <- (getDirectoryContents path >>= (readDirectoryContents path))
-    return $ Directory path contents                    
-
-readDirectoryContents :: Path -> [Path] -> IO [File]
-readDirectoryContents parent paths = sequence $ map readStatus $ map fullPath $ filter realFile $ paths
-    where
-          realFile path = not $ path `elem` ignored      
-          fullPath path = parent ++ "/" ++ path
-          
-readFileStatus :: Path -> IO File
-readFileStatus path = do
-    timestamp <- getModificationTime path
-    posixFileStatus <- getFileStatus path
-    return $ RegularFile path $ FileStatus timestamp (toInteger $ fileSize posixFileStatus)
-          
 readStatus :: Path -> IO File
 readStatus path = (doesDirectoryExist path) >>= (\dir -> if dir then (readDirectoryStatus path) else (readFileStatus path))
-                                
+    where
+        readFileStatus path = do
+            timestamp <- getModificationTime path
+            posixFileStatus <- getFileStatus path
+            return $ RegularFile path $ FileStatus timestamp (toInteger $ fileSize posixFileStatus)
+
+        readDirectoryStatus path = do      
+            contents <- (getDirectoryContents path >>= (readDirectoryContents path))
+            return $ Directory path contents                    
+
+        readDirectoryContents parent paths = sequence $ map readStatus $ map fullPath $ filter realFile $ paths
+            where
+                  realFile path = not $ path `elem` ignored      
+                  fullPath path = parent ++ "/" ++ path                               
