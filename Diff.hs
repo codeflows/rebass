67c2a0ca289466ac10d7ba4591e31ca38eb3893e
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-13 22:58:12 +0200

    diff seems feature complete... maybe test?

--- Diff.hs
+++ Diff.hs
@@ -10,11 +10,14 @@ diff (old:olds) [] = Removed old : diff olds []
 diff [] (new:news) = Added new : diff [] news
 diff (old:olds) (new:news) 
     | oldPath  < newPath = Removed old : diff olds (new:news)
-    | oldPath  > newPath = Added new : diff (old:olds) news
+    | oldPath  > newPath = findAdded new ++ diff (old:olds) news
     | otherwise          = compareFile old new ++ diff olds news
     where oldPath = pathOf old
           newPath = pathOf new
 
+findAdded :: File -> [Diff]
+findAdded new@(RegularFile _ _) = [Added new]
+findAdded new@(Directory _ contents) = Added new : (concat $ map findAdded contents)
 
 compareFile :: File -> File -> [Diff]
 compareFile (RegularFile _ _) new@(Directory _ _) = [Replaced new]
@@ -22,4 +25,5 @@ compareFile (Directory _ _) new@(RegularFile _ _) = [Replaced new]
 compareFile (RegularFile _ oldStatus) new@(RegularFile _ newStatus) 
     | (oldStatus == newStatus) = []
     | otherwise                = [Updated new]
-compareFile (Directory _ _) new@(Directory _ _) = []
+compareFile (Directory _ oldContents) new@(Directory _ newContents) 
+    = diff oldContents newContents
