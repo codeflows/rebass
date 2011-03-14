075e0ff8707db1b54cb8c629bfe49bc0e754c3c5
Jari Aarniala <jari.aarniala@gmail.com>
2011-03-14 22:32:17 +0200

    Wow, parsing <

--- ReaperProjectFileParser.hs
+++ ReaperProjectFileParser.hs
@@ -1,3 +1,19 @@
 -- Attempt at parsing Reaper project files with Parsec
 
+module Reaper where
+
 import Text.ParserCombinators.Parsec
+
+{-csvFile = endBy line eol
+line = sepBy cell (char ',')
+cell = many (noneOf ",\n")
+eol = char '\n'-}
+
+reaperNode = char '<'
+
+main    = do{ result <- parseFromFile reaperNode "Fake.RPP"
+              ; case result of
+                  Left err  -> print err
+                  Right xs  -> print xs
+              }
+
