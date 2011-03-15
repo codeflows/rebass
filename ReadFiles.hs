dabca1fe3da701d43651c3909f7adce0417b65db
Juha Paananen <juha.paananen@gmail.com>
2011-03-15 22:07:58 +0200

    use relative paths

--- ReadFiles.hs
+++ ReadFiles.hs
@@ -10,21 +10,25 @@ import Data.List(sort)
 ignored = [".", "..", ".git", ".gitignore", ".rebass"]
 
 readStatus :: Path -> IO File
-readStatus path = handleFileOrDirectory path readFileStatus readDirectoryStatus
+readStatus root = read root
     where
+        read path = handleFileOrDirectory path readFileStatus readDirectoryStatus
+
         readFileStatus path = do
             timestamp <- getModificationTime path >>= toCalendarTime
             posixFileStatus <- getFileStatus path
-            return $ RegularFile path $ FileStatus timestamp (toInteger $ fileSize posixFileStatus)
+            return $ RegularFile (relativePath path) $ FileStatus timestamp (toInteger $ fileSize posixFileStatus)
 
         readDirectoryStatus path = do      
             contents <- (getDirectoryContents path >>= (readDirectoryContents path))
-            return $ Directory path contents                    
+            return $ Directory (relativePath path) contents                    
 
-        readDirectoryContents parent paths = sequence $ map readStatus $ map fullPath $ filter realFile $ sort $ paths
+        readDirectoryContents parent paths = sequence $ map read $ map fullPath $ filter realFile $ sort $ paths
             where
                   realFile path = not $ path `elem` ignored      
                   fullPath path = parent ++ "/" ++ path
+        
+        relativePath path = drop ((length root)+1) path
                   
 handleFileOrDirectory path fileHandler directoryHandler = 
     (doesDirectoryExist path) >>= (\dir -> if dir then (directoryHandler path) else (fileHandler path))                  
