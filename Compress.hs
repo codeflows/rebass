9f86601e7d96fd4212626c958414bead15c3c3e5
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-16 13:11:13 +0200

    ready to do the actual compression

--- Compress.hs
+++ Compress.hs
@@ -4,4 +4,10 @@ import Diff
 import Path
 
 compress :: Path -> [Diff] -> IO ()
-compress path diffs = return ()
+compress path diffs = mapM_ (compressDiff path) diffs
+
+compressDiff root (Add path) = compressFile root path
+compressDiff root (Update path) = compressFile root path
+compressDiff _ _ = return ()
+
+compressFile root relative = return ()
