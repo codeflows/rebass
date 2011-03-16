146d18a904786ed747a167191498b8982e63db2a
Juha Paananen <juha.paananen@reaktor.fi>
2011-03-16 14:24:58 +0200

    add compression. full versions are still copied also

--- Compress.hs
+++ Compress.hs
@@ -2,6 +2,8 @@ module Compress where
 
 import Diff
 import Path
+import System.Process
+import List
 
 compress :: Path -> [Diff] -> IO ()
 compress path diffs = mapM_ (compressDiff path) diffs
@@ -10,4 +12,11 @@ compressDiff root (Add path) = compressFile root path
 compressDiff root (Update path) = compressFile root path
 compressDiff _ _ = return ()
 
-compressFile root relative = return ()
+compressFile root relative | ".wav" `elem` (tails relative) = do
+    putStrLn $ "Compressing " ++ relative
+    createProcess $ shell ("lame " ++ root ++ "/" ++ relative)
+    return ()
+    
+compressFile _ _ = return ()    
+    
+    
