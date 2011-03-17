ac06608742e544c2b8155b8a4554b2c428a21612
Juha Paananen <juha.paananen@gmail.com>
2011-03-17 23:22:38 +0200

    Finally only compressed files are copied. No mp3's are left in the local directory after compression

--- Compress.hs
+++ Compress.hs
@@ -4,27 +4,29 @@ import Diff
 import Path
 import System.Process
 import System.Exit
+import System.Directory(removeFile)
 import List
 import UpdateFile
+import Data.List.Utils (endswith, replace)
 
-compress :: Path -> [Diff] -> IO ()
-compress path diffs = mapM_ (compressDiff path) diffs
-
-compressDiff root (Add path) = compressFile (root `subPath` path)
-compressDiff root (Update path) = compressFile (root `subPath` path)
-compressDiff _ _ = return ()
-
-compressFile path | ".wav" `elem` (tails path) = do
-    putStrLn $ "Compressing " ++ path
-    result <- readProcessWithExitCode "lame" [path] []
+compressInto src dest = do
+    putStrLn $ "Compressing " ++ src
+    result <- readProcessWithExitCode "lame" [src, dest] []
     case result of
         (ExitSuccess, _, _) -> return ()
     
 compressFile _ = return ()
 
 compressAndUpdate :: Path -> Path -> Diff -> IO ()
-compressAndUpdate src dest diff = do
-    compressDiff src diff
-    updateFile src dest diff
-    
-    
+compressAndUpdate src dest diff | shouldConvert diff = compressAndCopy src dest $ pathOf diff
+    where shouldConvert (Add path) | endswith ".wav" path = True
+          shouldConvert (Update path) | endswith ".wav" path = True
+          shouldConvert _ = False
+compressAndUpdate src dest diff = updateFile src dest diff
+
+compressAndCopy :: Path -> Path -> Path -> IO ()
+compressAndCopy src dest path = do
+    let mp3Version = replace ".wav" ".mp3" path
+    compressInto (src `subPath` path) (src `subPath` mp3Version)
+    updateFile src dest $ Update mp3Version
+    removeFile (src `subPath` mp3Version) 
