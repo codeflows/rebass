8f57df24e0cdcadb5ef70702f54073398b01ddc2
Jari Aarniala <jari.aarniala@gmail.com>
2011-03-21 23:10:09 +0200

    Failing spec

--- ParserSpec.hs
+++ ParserSpec.hs
@@ -6,15 +6,16 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.Hspec.HUnit
 import Test.QuickCheck hiding (property)
-import Test.HUnit
 import ReaperProjectFileParser
 
 parserSpecs = describe "Minimal project file" [
     it "parses successfully"
-      (parse "<REAPER_PROJECT 0.1 \"3.73/OSX\">" == ReaperProject 0.1 "3.73/OSX")
-  ]
+      (parse "<REAPER_PROJECT 0.1 \"3.73/OSX\">" == Node ["0.1", "\"3.73/OSX\""])
+ ]
 
-parse :: String -> ReaperProject
+-- TODO handles values of different types (Float, String etc) properly
+
+parse :: String -> Node
 parse input =
   case parseProject input of
     Right r -> r
--- ReaperProjectFileParser.hs
+++ ReaperProjectFileParser.hs
@@ -1,19 +1,19 @@
 -- Attempt at parsing Reaper project files with Parsec
 
-module ReaperProjectFileParser(ReaperProject(ReaperProject), parseProject) where
+module ReaperProjectFileParser(Node(Node), parseProject) where
 
 import Text.ParserCombinators.Parsec
 
-data ReaperProject = ReaperProject Float String deriving (Show, Eq)
+data Node = Node [String] deriving (Show, Eq)
 
 name :: CharParser st String
 name = many1 (letter <|> char '_')
 
-project :: CharParser st ReaperProject
-project = do
+node :: CharParser st Node
+node = do
   char '<'
   n <- name
-  return $ ReaperProject 0.1 "3.73/OSX"
+  return $ Node [n]
 
-parseProject :: String -> Either ParseError ReaperProject
-parseProject input = parse project "no source file" input
+parseProject :: String -> Either ParseError Node
+parseProject input = parse node "(no source file)" input
