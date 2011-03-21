a8e5af33967bd62170a1b25af123b8a066cbc981
Jari Aarniala <jari.aarniala@gmail.com>
2011-03-21 21:36:08 +0200

    Back off a bit

--- ReaperProjectFileParser.hs
+++ ReaperProjectFileParser.hs
@@ -4,44 +4,8 @@ module Reaper where
 
 import Text.ParserCombinators.Parsec
 
-data Command = Command String [String] deriving (Show)
-data Node = Node Command [Command] deriving (Show)
-
--- TODO support different types of parameters, e.g. decimal numbers, strings in quotes etc
-parameter = many1 (noneOf " \n")
-
-parameters = sepBy1 parameter (char ' ')
-
-parameterList = do
-                  char ' '
-                  p <- parameters
-                  newline
-                  return p
-
-noParameters = do
-                newline
-                return [[]]
-
-maybeParameters = parameterList <|> noParameters
-
-name = many1 (letter <|> char '_')
-
-command = do
-            n <- name
-            p <- maybeParameters
-            return $ Command n p
-
-child = do
-          -- TODO getting unelegant
-          many (char ' ')
-          command
-
 node = do
-          char '<'
-          mainCommand <- command
-          subCommands <- many child
-          char '>'
-          return $ Node mainCommand subCommands
+  char '<'
 
 parseNode input = parse node "lolcat" input
 
