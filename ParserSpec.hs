74813b5e77504a4c8c1a6b90d74873b248b0544c
Jari Aarniala <jari.aarniala@gmail.com>
2011-03-22 01:03:35 +0200

    More reformatting

--- ParserSpec.hs
+++ ParserSpec.hs
@@ -11,30 +11,30 @@ parserSpecs = describe "Reaper project file parser" [
     it "parses minimal project definition"
       (assertParseResult
         "<REAPER_PROJECT\n>"
-        (Node (Command "REAPER_PROJECT" []) [])),
+        (Node
+          (Command "REAPER_PROJECT" []) [])),
 
     it "parses minimal project definition with parameters"
       (assertParseResult
         "<REAPER_PROJECT 0.1 \"3.73/OSX\"\n>"
-        (Node (Command "REAPER_PROJECT" ["0.1", "\"3.73/OSX\""]) [])),
+        (Node
+          (Command "REAPER_PROJECT" ["0.1", "\"3.73/OSX\""]) [])),
 
     it "parses project definition with one command"
       (assertParseResult
         "<REAPER_PROJECT\n  SAMPLERATE 44100 0\n>"
         (Node
           (Command "REAPER_PROJECT" [])
-          [Command "SAMPLERATE" ["44100", "0"]])),
+            [Command "SAMPLERATE" ["44100", "0"]])),
 
    it "parses project definition with many commands"
       (assertParseResult
         "<REAPER_PROJECT\n  SAMPLERATE 44100 0\n  LOCK 1\n>"
         (Node
           (Command "REAPER_PROJECT" [])
-          [Command "SAMPLERATE" ["44100", "0"],
-           Command "LOCK" ["1"]]))
+            [Command "SAMPLERATE" ["44100", "0"],
+             Command "LOCK" ["1"]]))
    ]
-
--- TODO <REAPER_PROJECT\n0.1 ... -> child nodes
 
 assertParseResult :: String -> Node -> HUnit.Assertion
 assertParseResult input expected = do
