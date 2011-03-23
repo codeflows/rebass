11de6f0f47b7eaf6808557a8685a1e68be6a1712
Jari Aarniala <jari.aarniala@gmail.com>
2011-03-24 00:25:57 +0200

    No need to ignore whitespace anymore when diffing.

--- parser.sh
+++ parser.sh
@@ -3,4 +3,4 @@ echo "Input parsed and then serialized:"
 runhaskell ParserMain.hs $1
 echo
 echo "Differences between original and rebassed:"
-runhaskell ParserMain.hs $1 | diff -w $1 -
+runhaskell ParserMain.hs $1 | diff $1 -
