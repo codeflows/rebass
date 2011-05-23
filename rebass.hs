4db25a292ee8e5c255470fea4c26dd779df3f83b
Juha Paananen <juha.paananen@gmail.com>
2011-05-23 15:03:19 +0300

    extract methods

--- rebass.hs
+++ rebass.hs
@@ -16,21 +16,40 @@ main = getArgs >>= rebass
 rebass :: [String] -> IO ()	
 
 rebass ["init", projectFile, remoteAlias] = do
+    dumpProject projectFile remoteAlias
+    status <- readStatus projectFile remoteAlias
+    writeStatus projectFile remoteAlias status 
+    putStrLn $ "Rebass initialized." 
+
+rebass ["update", remoteAlias] = do
+    putStrLn $ "Update not yet implemented!"
+
+rebass _ = do
+	putStrLn "USAGE:"
+	putStrLn "rebass init <projectfile> <remotealias>"
+	putStrLn "Example: rebass init examples/PatrolCar.RPP lollable"
+
+readStatus projectFile remoteAlias = do
+    remoteLocation <- remoteLocationFor remoteAlias
+    let projectName = lastPathElement projectFile
+    let remoteProjectFile = remoteLocation `subPath` projectName
+    localStatus <- projectStatus projectFile
+    remoteStatus <- projectStatus remoteProjectFile
+    return (localStatus, remoteStatus)
+
+writeStatus projectFile remoteAlias (localStatus, remoteStatus) = do
+    let projectName = lastPathElement projectFile
+    statusFile <- statusFileFor $ remoteAlias ++ "." ++ projectName ++ ".status"
+    createRebassDir
+    writeFile statusFile $ show (localStatus, remoteStatus)
+    putStrLn $ "Using status file " ++ statusFile
+    writeFile statusFile $ show (localStatus, remoteStatus)
+
+dumpProject projectFile remoteAlias = do
     remoteLocation <- remoteLocationFor remoteAlias
     let projectName = lastPathElement projectFile
     createDirectoryIfMissing True remoteLocation
     flattenSamples projectFile remoteLocation	
     let remoteProjectFile = remoteLocation `subPath` projectName
     parseProjectFile projectFile >>= (writeFile remoteProjectFile) . serialize . flatten
-    localStatus <- projectStatus projectFile
-    remoteStatus <- projectStatus remoteProjectFile
-    statusFile <- statusFileFor $ remoteAlias ++ "." ++ projectName ++ ".status"
-    createRebassDir
-    writeFile statusFile $ show (localStatus, remoteStatus)
-    putStrLn $ "Using status file " ++ statusFile
-    putStrLn $ "Rebass initialized. Using remote repository " ++ remoteLocation    
 
-rebass _ = do
-	putStrLn "USAGE:"
-	putStrLn "rebass init <projectfile> <remotealias>"
-	putStrLn "Example: rebass init examples/PatrolCar.RPP lollable"
