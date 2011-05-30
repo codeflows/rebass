eb1ff6578b5261662dc8d3e1f196e4b691f0368a
Juha Paananen <juha.paananen@gmail.com>
2011-05-30 23:34:48 +0300

    cleanup

--- src/Rebass/Cache.hs
+++ src/Rebass/Cache.hs
@@ -5,30 +5,12 @@ import System.Directory(createDirectoryIfMissing, getHomeDirectory)
 import Rebass.Path
 import Control.Monad(liftM)
 
-saveRemote :: String -> IO String
-saveRemote name = do
-    remote <- remoteLocationFor name
-    createRebassDir
-    createDirectoryIfMissing True remote
-    putStrLn $ "Created directory " ++ remote
-    remoteConfig <- remoteConfigFile
-    writeFile remoteConfig remote
-    return remote
-
 remoteLocationFor name = do
     home <- getHomeDirectory
     return $ home ++ "/Dropbox/Rebass/" ++ name
     
-statusFileFor name = rebassDir >>= returnSubPath name
+statusFileFor name = fmap (`subPath` name) rebassDir
 	
-getRemote = remoteConfigFile >>= readFile	
-
 createRebassDir = rebassDir >>= createDirectoryIfMissing True
 
 rebassDir = return ".rebass"
-
-statusFile = rebassDir >>= returnSubPath "status"
-
-remoteConfigFile = rebassDir >>= returnSubPath "remote"
-
-returnSubPath file dir = return $ dir `subPath` file
